0130500256590841b472b7fdc658c2cb18644f64
IAG01 <rm93971@estudante.fieb.edu.br>
2025-06-02 14:07:11 +0000

    menu

---
+++ src/Menu.jsx
@@ -0,0 +1,17 @@
+import './Menu.css'
+
+const Menu =()=>{
+
+    return(
+        <div>
+            <ul class="nav">
+            <li class="nav-item">
+            <a class="nav-link activate" href="#">
+               Home
+              </a>
+           </li>     
+        </ul>
+    </div>
+    )
+}
+export default Menu
